c989d1733ebf5411bf1afc41a56986a2d0f001ca
Gouranga123456789 <gourangakalita17@gmail.com>
2025-11-25 11:32:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,22 +1,36 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path'; 
+import { fileURLToPath } from 'url'; 
+
 import authRoutes from './src/routes/auth.routes.js';
 import proposalRoutes from './src/routes/proposal.routes.js';
 import investorRoutes from './src/routes/investor.routes.js';
 import bankerRoutes from './src/routes/banker.routes.js';
 
 dotenv.config();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors()); 
-app.use(express.json()); 
+app.use(cors());
+app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/proposals', proposalRoutes);
 app.use('/api/investor-proposals', investorRoutes);
-app.use('/api/loans', bankerRoutes); 
+app.use('/api/loans', bankerRoutes);
+
+const frontendPath = path.join(__dirname, '../frontend/dist');
+
+app.use(express.static(frontendPath));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
